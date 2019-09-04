31ae8aefe63085b0f9ba79c143d9b7334c5d91dc
“AhmadFauzi-28” <ahmadfauzi282002@gmail.com>
2019-09-04 22:50:54 +0700

    TeFa-Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const port = 9000;
+const port = process.env.PORT || 9000;
 
-http.createServer(Function(req,res){
+http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
     res.end('{"success":true, "data":"Index", "message":"Hello World", "code":200}');
 }).listen(port, '127.0.0.1');
